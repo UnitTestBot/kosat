45b8939cc945211682d2496975604c81d61f381c
AlekseyVlasov <vlasoff.al03@gmail.com>
2022-07-07 14:40:29 +0300

    CDCL class with function solve. Without realization of analyse and backjump

--- src/commonMain/kotlin/org/kosat/SatSolver.kt
+++ src/commonMain/kotlin/org/kosat/SatSolver.kt
@@ -5,13 +5,13 @@ import kotlinx.collections.immutable.persistentListOf
 import kotlin.math.abs
 
 //DPLL
-fun solveCnf(cnf: CnfRequest) : List<Int>? {
+fun solveCnf(cnf: CnfRequest): List<Int>? {
     val clauses = ArrayList(cnf.clauses.map { ArrayList(it.lit.toList()) })
     return dpll(clauses)?.sortedBy { abs(it) }
 }
 
 
-fun unitPropagate(clauses: ArrayList<ArrayList<Int>>) : List<Int>? {
+fun unitPropagate(clauses: ArrayList<ArrayList<Int>>): List<Int>? {
     val res = mutableListOf<Int>()
     while (true) {
         // If a clause is a unit clause, i.e. it contains only a single unassigned literal, this clause can only be
@@ -27,13 +27,13 @@ fun unitPropagate(clauses: ArrayList<ArrayList<Int>>) : List<Int>? {
     }
 }
 
-fun chooseLiteral(clauses: ArrayList<ArrayList<Int>>) : Int {
+fun chooseLiteral(clauses: ArrayList<ArrayList<Int>>): Int {
     //dumb strategy, place your heuristics here
     return clauses.first()[0]
 }
 
 //returns false only if after substitution some clause becomes empty
-fun substitute(clauses: ArrayList<ArrayList<Int>>, literal: Int) : Boolean {
+fun substitute(clauses: ArrayList<ArrayList<Int>>, literal: Int): Boolean {
     //removing every clause containing literal
     clauses.removeAll { it.contains(literal) }
 
@@ -82,4 +82,73 @@ fun dpll(clauses: ArrayList<ArrayList<Int>>): PersistentList<Int>? {
     }
 
     return null
+}
+
+class CDCL(private var clauses: ArrayList<ArrayList<Int>>, private val varsNumber: Int) {
+    // clause is where this literal came from (if it 'guessed' literal than clause == -1)
+    data class TrailMember(val literal: Int, val clause: Int, val decisionLevel: Int)
+    enum class LitStatus { TRUE, FALSE, UNDEFINED }
+
+    // values of variables
+    private var litValues: MutableList<LitStatus> = MutableList(varsNumber + 1) { LitStatus.UNDEFINED }
+
+    // all decisions and consequences
+    private val trail: ArrayList<TrailMember> = ArrayList()
+
+    // decision level
+    private var level: Int = 0
+    fun solve(): List<Int>? {
+        while (true) {
+            // if conflict
+            if (unitPropagateCdcl()) {
+                if (level == 0) return null
+                val newClause: ArrayList<Int> = analyzeConflict()
+                addClause(newClause)
+                backjump(newClause)
+            } else {
+                // return model if all variables have value
+                if (trail.size == varsNumber) return trail.map { it.literal }
+
+                // make new decision
+                level++
+                // todo: choose undefined literal
+                val chosenLit = chooseLiteral(clauses)
+                trail.add(TrailMember(chosenLit, -1, level))
+            }
+        }
+    }
+
+    // change level, undefine variables and so on
+    private fun backjump(newClause: ArrayList<Int>) {
+        // todo
+    }
+
+    // add clause and change structures for it
+    private fun addClause(newClause: ArrayList<Int>) {
+        // todo
+        clauses.add(newClause)
+    }
+
+    // return true if you get conflict
+    // todo: norm realization
+    private fun unitPropagateCdcl(): Boolean {
+        for (clause in clauses.indices) {
+            val undefined = ArrayList<Int>()
+            for (literal in clauses[clause]) {
+                if (litValues[abs(literal)] == LitStatus.UNDEFINED) undefined.add(literal)
+            }
+            if (undefined.size == 1) {
+                val literal = undefined[0]
+                trail.add(TrailMember(literal, clause, level))
+                litValues[abs(literal)] = if (literal > 0) LitStatus.TRUE else LitStatus.FALSE
+            }
+        }
+        return false
+    }
+
+    // analyze conflict and return new clause
+    private fun analyzeConflict(): ArrayList<Int> {
+        // todo: hardest one
+        return ArrayList()
+    }
 }
