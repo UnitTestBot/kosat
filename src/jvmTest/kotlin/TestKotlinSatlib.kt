bbfa763ceda495f89723a858792b07f6105b05de
Konstantin Chukharev <lipen00@gmail.com>
2022-07-11 18:16:48 +0300

    Fix usage of kotlin-satlib, delete unnecessary native libs, move deps

---
+++ src/jvmTest/kotlin/TestKotlinSatlib.kt
@@ -0,0 +1,42 @@
+import com.github.lipen.satlib.op.exactlyOne
+import com.github.lipen.satlib.solver.MiniSatSolver
+import com.github.lipen.satlib.solver.solve
+import org.junit.jupiter.api.Test
+
+internal class TestKotlinSatlib {
+    @Test
+    fun runMiniSat() {
+        with(MiniSatSolver()) {
+            val x = newLiteral()
+            val y = newLiteral()
+            val z = newLiteral()
+
+            println("Encoding exactlyOne(x, y, z)")
+            exactlyOne(x, y, z)
+
+            println("nVars = $numberOfVariables")
+            println("nClauses = $numberOfClauses")
+
+            println("Solving...")
+            check(solve())
+            println("model = ${getModel()}")
+
+            println("Solving with assumption x=true...")
+            check(solve(x))
+            println("model = ${getModel()}")
+            check(getValue(x))
+
+            println("Solving with assumption y=true...")
+            check(solve(y))
+            println("model = ${getModel()}")
+            check(getValue(y))
+
+            println("Solving with assumption z=true...")
+            check(solve(z))
+            println("model = ${getModel()}")
+            check(getValue(z))
+
+            println("Everything OK.")
+        }
+    }
+}
