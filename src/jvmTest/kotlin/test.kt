6c842a117500d38b2fe6a0d37bb0704d9938bb9e
daver <averkov.daniil@yandex.ru>
2022-07-11 15:52:00 +0300

    - more tests - different test system

--- src/jvmTest/kotlin/test.kt
+++ src/jvmTest/kotlin/test.kt
@@ -5,11 +5,12 @@ import kotlin.system.measureTimeMillis
 import com.github.lipen.satlib.solver.MiniSatSolver
 import org.kosat.Clause
 import org.kosat.solveCnf
+import java.nio.file.Files
+import java.nio.file.Paths
 import kotlin.math.abs
 import kotlin.math.sign
+import kotlin.streams.toList
 
-val packageName = "src/jvmTest/resources/"
-val strLen = 13
 
 fun fill(s: String): String {
     return s.padEnd(strLen, ' ')
@@ -47,33 +48,47 @@ fun checkKoSatSolution(ans: List<Int>?, input: String, isSolution: Boolean): Boo
     return !isFailedClause
 }
 
+val projectDirAbsolutePath = Paths.get("").toAbsolutePath().toString()
+val packageName = "src/jvmTest/resources/"
+val strLen = 13
+
+fun getAllFilenamesInResources() : List<String> {
+    val resourcesPath = Paths.get(projectDirAbsolutePath, packageName)
+    return Files.walk(resourcesPath)
+        .filter { item -> Files.isRegularFile(item) }
+        .map { item -> item.toString().substring(projectDirAbsolutePath.length + packageName.length + 1) }.toList()
+}
+
 internal class DiamondTests {
-    val testNumber = 18
+    val filenames: List<String> = getAllFilenamesInResources()
+    val testNumber = filenames.size
     val groupName = "diamond"
     val name = packageName + groupName
     val format = ".cnf"
 
     @Test
     fun diamondTests() {
+        println(filenames)
         println("${fill("Name:")} | ${fill("KoSat time:")} | ${fill("MiniSat time:")} | ${fill("Check result")} | ${fill("Solvable")}")
         MiniSatSolver()
         for (ind in 1..testNumber) {
-            val filename = name + ind + format
-            val testName = groupName + ind
+            val filename = packageName + filenames[ind - 1]
+            val testName = filenames[ind - 1].substring(0, filenames[ind - 1].length - format.length)
 
             val input = File(filename).readText()
 
             var solution: List<Int>?
             var isSolution: Boolean
 
-            val timeKoSat: Double = measureTimeMillis { solution = solveCnf(readCnfRequests(input).first()) }.toDouble() / 1000
+            val timeKoSat = measureTimeMillis { solution = solveCnf(readCnfRequests(input).first()) }.toDouble() / 1000
+
             val timeMiniSat: Double = measureTimeMillis { isSolution = processMiniSatSolver(input) }.toDouble() / 1000
 
             val checkRes = if (checkKoSatSolution(solution, input, isSolution)) "OK" else "WA"
 
-
-
             println("${fill(testName)} | ${fill(timeKoSat.toString())} | ${fill(timeMiniSat.toString())} | ${fill(checkRes)} | ${if(isSolution) "SAT" else "UNSAT"}")
+
         }
     }
 }
+
