e25c15a799f66a821f8f6ba73cba5e50fa3fcd71
AlekseyVlasov <vlasoff.al03@gmail.com>
2022-07-13 18:08:01 +0300

    style fixes

--- src/jvmMain/kotlin/org/kosat/Main.kt
+++ src/jvmMain/kotlin/org/kosat/Main.kt
@@ -1,12 +1,5 @@
 package org.kosat
 
-import com.github.lipen.satlib.card.declareCardinality
-import com.github.lipen.satlib.op.exactlyOne
-import com.github.lipen.satlib.solver.MiniSatSolver
-import com.github.lipen.satlib.solver.addClause
-import com.github.lipen.satlib.solver.solve
-import java.io.File
-
 
 fun main() {
     println("v KOSAT SAT SOLVER, v1.0")
