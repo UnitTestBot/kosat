c69fbdf7614a31e79861d0a47228a4862abff439
AlekseyVlasov <vlasoff.al03@gmail.com>
2022-08-03 04:24:35 +0300

    delegate clause by lits

--- src/commonMain/kotlin/org/kosat/Clause.kt
+++ src/commonMain/kotlin/org/kosat/Clause.kt
@@ -2,7 +2,7 @@ package org.kosat
 
 typealias Lit = Int
 
-class Clause(val lits: MutableList<Lit>): Collection<Lit> {
+class Clause(val lits: MutableList<Lit>): Collection<Lit> by lits {
 
     constructor(): this(mutableListOf<Lit>())
 
@@ -12,22 +12,11 @@ class Clause(val lits: MutableList<Lit>): Collection<Lit> {
 
     var lbd = 0
 
-    override operator fun iterator() = lits.iterator()
-
     operator fun get(index: Int) = lits[index]
 
     operator fun set(index: Int, lit: Lit) {
         lits[index] = lit
     }
-
-    override val size: Int
-        get() = lits.size
-
-    override fun containsAll(elements: Collection<Lit>): Boolean = lits.containsAll(elements)
-
-    override fun contains(element: Lit): Boolean = lits.contains(element)
-
-    override fun isEmpty(): Boolean = lits.isEmpty()
 
     fun add(lit: Int) {
         lits.add(lit)
