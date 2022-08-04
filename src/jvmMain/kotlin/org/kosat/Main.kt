9e938ae9d6bdf6bce5babc434f6a63c884ce3b5d
AlekseyVlasov <vlasoff.al03@gmail.com>
2022-08-04 12:04:57 +0300

    input from file with right configuration

--- src/jvmMain/kotlin/org/kosat/Main.kt
+++ src/jvmMain/kotlin/org/kosat/Main.kt
@@ -1,11 +1,17 @@
 package org.kosat
 
+import java.io.File
 import kotlin.system.measureTimeMillis
 
-fun main() {
+fun main(args: Array<String>) {
     println("v KOSAT SAT SOLVER, v1.0")
     println("v Solves formulas in CNF form. Input must as formatted according simplified DIMACS specified in http://www.satcompetition.org/2004/format-solvers2004.html")
-    val input = System.`in`.bufferedReader().readText()
+    var input = ""
+    if (args[0] == "--file") {
+        input = File(args[1]).readText()
+    } else {
+        input = System.`in`.bufferedReader().readText()
+    }
     var res = ""
     val time = measureTimeMillis { res = processCnfRequests(readCnfRequests(input)) }.toDouble()/1000
 
