ec4a4d68b1c98bbe2060f13e93666500159874fb
MusinaPolina <pmusina03@gmail.com>
2022-08-08 16:09:18 +0300

    inheritance (clause.kt by)

--- src/commonMain/kotlin/org/kosat/Clause.kt
+++ src/commonMain/kotlin/org/kosat/Clause.kt
@@ -2,24 +2,7 @@ package org.kosat
 
 typealias Lit = Int
 
-class Clause(val lits: MutableList<Lit>) : Collection<Lit> by lits {
-
-    constructor() : this(mutableListOf<Lit>())
-
+class Clause(val lits: MutableList<Lit>): MutableList<Lit> by lits {
     var deleted = false
-
     var lbd = 0
-
-    operator fun get(index: Int) = lits[index]
-
-    operator fun set(index: Int, lit: Lit) {
-        lits[index] = lit
-    }
-
-    fun add(lit: Int) {
-        lits.add(lit)
-    }
-
-    val lastIndex: Int
-        get() = lits.lastIndex
 }
