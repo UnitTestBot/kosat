c89e21b4eb688f0a2456be7ed21421a46d021353
Konstantin Chukharev <lipen00@gmail.com>
2023-07-10 14:20:58 +0300

    Use Libs

--- kosat-core/build.gradle.kts
+++ kosat-core/build.gradle.kts
@@ -11,7 +11,7 @@ kotlin {
     sourceSets {
         commonMain {
             dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
+                implementation(Libs.KotlinxCollectionsImmutable.kotlinx_collections_immutable)
             }
         }
 
--- kosat-web/build.gradle.kts
+++ kosat-web/build.gradle.kts
@@ -11,7 +11,7 @@ kotlin {
 
 dependencies {
     implementation(project(":core"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.3")
+    implementation(Libs.KotlinxCoroutines.kotlinx_coroutines_core)
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react:17.0.2-pre.290-kotlin-1.6.10")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:17.0.2-pre.290-kotlin-1.6.10")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-css:17.0.2-pre.290-kotlin-1.6.10")
