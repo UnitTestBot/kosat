ac103d5fb9c1bf02012e6193214d321da9199f06
Konstantin Chukharev <lipen00@gmail.com>
2023-07-11 12:40:56 +0300

    Move tests to kosat-core

---
+++ kosat-core/src/commonMain/kotlin/org/kosat/cnf/CNF.kt
@@ -0,0 +1,59 @@
+package org.kosat.cnf
+
+import okio.BufferedSource
+
+class CNF(
+    val clauses: List<List<Int>>,
+    val numVars: Int = determineNumberOfVariables(clauses),
+) {
+    companion object {
+        private val RE_SPACE: Regex = """\s""".toRegex()
+
+        fun from(source: BufferedSource): CNF {
+            val clauses: MutableList<List<Int>> = mutableListOf()
+            var numVars = 0
+
+            while (true) {
+                val line = source.readUtf8Line() ?: break
+
+                if (line.startsWith('c')) {
+                    // Skip comment
+                } else if (line.startsWith('p')) {
+                    // Header
+                    val tokens = line.split(RE_SPACE)
+                    check(tokens[0] == "p") {
+                        "First header token must be 'p': \"$line\""
+                    }
+                    check(tokens[1] == "cnf") {
+                        "Second header token must be 'cnf': \"$line\""
+                    }
+                    check(tokens.size == 4) {
+                        "Header should have exactly 3 tokens: \"$line\""
+                    }
+                    numVars = tokens[2].toInt()
+                } else {
+                    // Clause
+                    val tokens = line.split(RE_SPACE)
+                    check(tokens.last() == "0")
+                    val lits = tokens.map { it.toInt() }
+                    val clause = lits.dropLast(1)
+                    clauses.add(clause)
+                }
+            }
+
+            val determinedNumVars = determineNumberOfVariables(clauses)
+            if (numVars == 0) {
+                numVars = determinedNumVars
+            }
+            if (determinedNumVars > numVars) {
+                error("CNF contains more variables ($determinedNumVars) than specified in the header ($numVars)")
+            }
+
+            return CNF(clauses, numVars)
+        }
+    }
+}
+
+fun determineNumberOfVariables(clauses: List<List<Int>>): Int {
+    return clauses.maxOfOrNull { it.max() } ?: 0
+}
--- kosat-core/src/jvmTest/kotlin/org/kosat/DiamondTests.kt
+++ kosat-core/src/jvmTest/kotlin/org/kosat/DiamondTests.kt
@@ -1,16 +1,10 @@
+package org.kosat
+
 import com.github.lipen.satlib.solver.MiniSatSolver
 import korlibs.time.measureTimeWithResult
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
-import org.kosat.CDCL
-import org.kosat.DimacsLiteral
-import org.kosat.LBool
-import org.kosat.Model
-import org.kosat.get
-import org.kosat.readCnfRequests
-import org.kosat.round
-import org.kosat.solveCnf
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -176,7 +170,7 @@ internal class DiamondTests {
         return res != "WA"
     }
 
-    private val testsPath = "src/test/resources"
+    private val testsPath = "src/jvmTest/resources"
 
     @ParameterizedTest(name = "{0}")
     @MethodSource("getAllFilenames")
