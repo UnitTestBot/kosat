3f5e645c34e140d7884a71b6033b8011a15ea78b
Nikolay Stepanov <elteammate@gmail.com>
2023-07-20 10:45:03 +0300

    Move sortDedupAndCheckComplimentary to util.kt

--- kosat-core/src/commonMain/kotlin/org/kosat/CDCL.kt
+++ kosat-core/src/commonMain/kotlin/org/kosat/CDCL.kt
@@ -190,32 +190,6 @@ class CDCL {
         }
     }
 
-    // ---- Utility methods ---- //
-
-    /**
-     * Takes a list of literals, sorts it and removes duplicates in place,
-     * then checks if the list contains a literal and its negation
-     * and returns true if so.
-     */
-    private fun sortDedupAndCheckComplimentary(lits: MutableList<Lit>): Boolean {
-        lits.sortBy { it.inner }
-
-        var i = 0
-        for (j in 1 until lits.size) {
-            if (lits[i] == lits[j].neg) return true
-            if (lits[i] != lits[j]) {
-                i++
-                lits[i] = lits[j]
-            }
-        }
-
-        while (lits.size > i + 1) {
-            lits.removeLast()
-        }
-
-        return false
-    }
-
     // ---- Trail ---- //
 
     fun backtrack(level: Int) {
