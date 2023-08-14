69c4cd2682aa5fb0d22416ac99d54770b8c5465e
Nikolay Stepanov <elteammate@gmail.com>
2023-08-14 13:02:23 +0300

    Switch to kotlin multiplatform

--- kosat-web/build.gradle.kts
+++ kosat-web/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("js")
+    kotlin("multiplatform")
 }
 
 kotlin {
@@ -7,17 +7,20 @@ kotlin {
         browser()
         binaries.executable()
     }
-}
 
-dependencies {
-    implementation(project(":core"))
-    implementation(Libs.KotlinxCoroutines.kotlinx_coroutines_core)
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-react:17.0.2-pre.290-kotlin-1.6.10")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:17.0.2-pre.290-kotlin-1.6.10")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-css:17.0.2-pre.290-kotlin-1.6.10")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-mui:5.2.7-pre.290-kotlin-1.6.10")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-mui-icons:5.2.5-pre.290-kotlin-1.6.10")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-emotion:11.7.1-pre.290-kotlin-1.6.10")
-    implementation(npm("@emotion/react", "11.7.1"))
-    implementation(npm("@emotion/styled", "11.6.0"))
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation(project(":core"))
+                implementation(Libs.KotlinxCoroutines.kotlinx_coroutines_core)
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-react:17.0.2-pre.290-kotlin-1.6.10")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:17.0.2-pre.290-kotlin-1.6.10")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-react-css:17.0.2-pre.290-kotlin-1.6.10")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-mui:5.2.7-pre.290-kotlin-1.6.10")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-emotion:11.7.1-pre.290-kotlin-1.6.10")
+                implementation(npm("@emotion/react", "11.7.1"))
+                implementation(npm("@emotion/styled", "11.6.0"))
+            }
+        }
+    }
 }
