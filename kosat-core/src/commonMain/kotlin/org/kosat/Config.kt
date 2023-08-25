8c82a40f930794e91d4c80dd9b199d517b5c8185
Nikolay Stepanov <elteammate@gmail.com>
2023-08-25 15:25:42 +0300

    Basic config structure

---
+++ kosat-core/src/commonMain/kotlin/org/kosat/Config.kt
@@ -0,0 +1,29 @@
+package org.kosat
+
+data class Config(
+    var restarterLubyConstant: Int = 50,
+
+    var clauseDbStrategy: ReduceStrategy = ReduceStrategy.LBD,
+    var clauseDbMaxSizeInitial: Int = 6000,
+    var clauseDbMaxSizeIncrement: Int = 500,
+    var clauseDbActivityDecay: Double = 0.999,
+
+    var vsidsActivityIncrement: Double = 1.0,
+    var vsidsActivityMultiplier: Double = 1.1,
+
+    var els: Boolean = true,
+    var elsRoundsBeforeFlp: Int = 5,
+    var elsRoundsAfterFlp: Int = 5,
+
+    var flp: Boolean = true,
+    var flpMaxProbes: Int = 1000,
+    val flpHyperBinaryResolution: Boolean = true,
+
+    var bve: Boolean = true,
+    var bveResolventSizeLimit: Int = 16,
+    var bveVarScoreSumWeight: Double = -1.0,
+    var bveVarScoreProdWeight: Double = 1.0,
+    var bveMaxVarScore: Double = 1.0,
+    var bveMaxNewResolventsPerElimination: Int = 16,
+    var bveMaxVarsToEliminate: Int = Int.MAX_VALUE,
+)
