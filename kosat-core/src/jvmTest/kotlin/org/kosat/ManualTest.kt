6b92dce882fc9b718379e2fdbeeb5345e14ebbd9
Konstantin Chukharev <lipen00@gmail.com>
2023-08-29 17:31:55 +0300

    Update manual test

--- kosat-core/src/jvmTest/kotlin/org/kosat/ManualTest.kt
+++ kosat-core/src/jvmTest/kotlin/org/kosat/ManualTest.kt
@@ -13,7 +13,8 @@ internal class ManualTest {
     fun testManual() {
         // val path = "src/jvmTest/resources/testCover/small/prime4.cnf".toPath()
         // val path = "../data/satcomp-2017/mp1-squ_ali_s10x10_c39_bail_SAT.cnf".toPath()
-        val path = "../data/satcomp-2017/mp1-9_1.cnf".toPath()
+        // val path = "../data/satcomp-2017/mp1-9_1.cnf".toPath()
+        val path = "../data/suite/mp1-ps_5000_21250_3_0_0.8_0_1.50_0.cnf".toPath()
         val cnf = CNF.from(path)
         val clauses = cnf.clauses.map { lits ->
             Clause(LitVec(lits.map { Lit.fromDimacs(it) }))
